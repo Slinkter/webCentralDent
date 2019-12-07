902472cee5e386578630912aa579bf6c61494359
Slinkter <luis.j.cueva@gmail.com>
2019-12-07 12:27:27 -0500

    Sabado pre idiomas

--- enviar.php
+++ enviar.php
@@ -4,6 +4,9 @@ $form_name = $_POST["form_name"];
 $form_tel = $_POST["form_tel"];
 $form_msj = $_POST["form_msj"];
 
+$secret = $_POST["secret"];
+$response = $_POST["response"];
+
 $para      = 'consultas@limacentraldent.com';
 $titulo    = 'Correo de  Web';
 $mensaje   = 'Nombre : '.$form_name  ."\n".             
@@ -14,6 +17,10 @@ $cabeceras = 'From: webmaster@limacentraldent.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($para, $titulo, $mensaje, $cabeceras);
+$url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secret."&response=".$response;
+$verify = file_get_contents($url);
+
+echo $verify;
 header("Location:gracias.html");
 
 
